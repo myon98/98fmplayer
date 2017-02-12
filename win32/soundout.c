5065868b997912ff6caf6060f4198aafcc84b404
Takamichi Horikawa <takamichiho@gmail.com>
2017-02-12 11:12:26 +0900

    experimental wasapi out (disabled, doesn't work on wine)

--- win32/soundout.c
+++ win32/soundout.c
@@ -1,10 +1,17 @@
 #include "soundout.h"
 #include "dsoundout.h"
 #include "waveout.h"
+#include "wasapiout.h"
 
 struct sound_state *sound_init(HWND hwnd, unsigned srate, unsigned sectlen,
                                sound_callback cbfunc, void *userptr) {
   struct sound_state *state;
+  /*
+  state = wasapi_init(hwnd, srate, sectlen, cbfunc, userptr);
+  if (state) {
+    return state;
+  }
+  */
   state = dsound_init(hwnd, srate, sectlen, cbfunc, userptr);
   if (state) {
     return state;
