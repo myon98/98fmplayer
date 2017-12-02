00f1d8d6cacf48139b976f3a25c040d51e1abe74
Takamichi Horikawa <takamichiho@gmail.com>
2017-12-02 14:42:47 +0900

    updated sdl

--- sdl/main.c
+++ sdl/main.c
@@ -152,9 +152,13 @@ int main(int argc, char **argv) {
     SDL_Quit();
     return 1;
   }
-
-  g.fp = fmdsp_pacc_init(pc, &pacc);
+  g.fp = fmdsp_pacc_alloc();
   if (!g.fp) {
+    SDL_Log("Cannot initialize fmdsp\n");
+    SDL_Quit();
+    return 1;
+  }
+  if (!fmdsp_pacc_init(g.fp, pc, &pacc)) {
     SDL_Log("Cannot initialize fmdsp\n");
     SDL_Quit();
     return 1;
